32c7131c6a20b20b194cd165ae46f04ef9a8fa11
Lovesomemadara <borovkun@gmail.com>
2024-02-09 13:36:53 +0500

    Задания выполнил!

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,1 +1,17 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+line: list[str, ...] = input().split()
+
+# if not line:
+#     print('Пустой список')
+# else:
+#     for index, item in enumerate(line):
+#         print(f'{int(item)} {index}')
+
+length: int = len(line)
+
+if length == 0:
+    print('Пустой список')
+else:
+    i: int = 0
+    while i < length:
+        print(f'{line[i]} {i}')
+        i += 1
--- tasks/task_1.py
+++ tasks/task_1.py
@@ -1,1 +1,16 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+values: list[str, ...] = []
+
+while True:
+    value: str = input()
+    stop_flag = value.lower() == 'конец'
+    if values and stop_flag:
+        break
+    elif not values and stop_flag:
+        print('Ничего не введено')
+        break
+    else:
+        values.append(value)
+
+if values:
+    result: str = ','.join(values)
+    print(result)
