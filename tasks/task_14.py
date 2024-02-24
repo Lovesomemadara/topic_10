4d77d0ce3496f34006c6115455ee235f6986b5f6
Danil <lovesomemadara@gmail.com>
2024-02-24 13:46:24 +0500

    Задание 15 - изменил вывод за "год", с 4.09 на 4.34 Задания выполнил

--- tasks/task_14.py
+++ tasks/task_14.py
@@ -1,1 +1,16 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+nums: list[int, ...] = [int(x) for x in input().split()]
+
+merged_tiles: list[int, ...] = [0] * len(nums)
+i: int = 0
+for tile in nums:
+    if tile != 0:
+        if merged_tiles[i] == 0:
+            merged_tiles[i] = tile
+        elif merged_tiles[i] == tile:
+            merged_tiles[i] *= 2
+            i += 1
+        else:
+            i += 1
+            merged_tiles[i] = tile
+
+print(merged_tiles)
--- tasks/task_16.py
+++ tasks/task_16.py
@@ -1,1 +1,12 @@
-# TODO: Пожалуйста, добавьте свой код ниже с комментариями и понятными названиями переменных.
+nums: list[int, ...] = [int(x) for x in input().split('x')]
+
+array: list[str, ...] = ['*' * nums[-1]]
+for size in nums[::-1][1:]:
+    inner_array: list[str, ...] = [array] * size
+    array: list[str, ...] = inner_array
+
+for sublist in array:
+    for line in sublist:
+        print(' '.join(line))
+    if len(nums) == 3:
+        print()
